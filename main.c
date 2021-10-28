b76444783d9c6411ed1f86d4691ae667b602d45a
zawatin <zawazawanomorichan@outlook.jp>
2021-10-28 19:41:02 +0900

    main.cに階上を実装

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int a, b;
+    int a, b, i, ans = 1;
     char c;
 
     scanf("%d %c %d",&a,&c,&b);
@@ -16,14 +17,26 @@ int main() {
 
 
     if (c == '/') {
-        if(b == 0) print("Error!!!\n");
-        print("%lf\n", (double)a / (double)b);
+        if(b == 0) printf("Error!!!\n");
+        printf("%lf\n", (double)a / (double)b);
     }
 	
 
     if(c=='*'){
 		printf("%d\n",a*b);
 	}
+
+    if (c == 'p') {
+        if (a < b) {
+            printf("error!\n");
+            exit(1);
+        }
+
+        for (i = a ; i > a - b ; i--)
+            ans *= i;
+
+        printf("%d\n", ans);
+    }
 
 	return 0;
 }
